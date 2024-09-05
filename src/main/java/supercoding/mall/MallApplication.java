0890aa5cda1fc1d3754699f82906d1634c055a21
lsy28901 <tndus28901@gmail.com>
2024-09-05 17:04:38 +0900

    🔰[Init] 새로운 프로젝트

--- src/main/java/supercoding/mall/MallApplication.java
+++ src/main/java/supercoding/mall/MallApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MallApplication {
-
+    //make
     public static void main(String[] args) {
         SpringApplication.run(MallApplication.class, args);
     }
